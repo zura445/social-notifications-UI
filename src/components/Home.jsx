204cdc4a87e614c8d03a25e22bda1e2d32ff825f
zura tetrashvili <zuratetra@gmail.com>
2024-06-19 15:02:50 +0400

    bug fixing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,48 +18,52 @@ export default function Home({ notifications, setNotifications }) {
           }`}
           onClick={() => handleNotificationClick(notification.id)}
         >
-          <div className="notification-details flex items-center my-2 ml-5">
-            <img
-              src={notification.profilePicture}
-              alt={notification.name}
-              className="w-[45px] h-[45px]"
-            />
-            <div className="ml-5">
-              <div className="flex items-center">
+          <div className="notification-details flex items-center my-2 ml-5 flex-row justify-between w-full pr-2">
+            <div className="flex items-center">
+              <img
+                src={notification.profilePicture}
+                alt={notification.name}
+                className="w-[45px] h-[45px]"
+              />
+              <div className="ml-5">
                 <div className="flex items-center">
-                  <h3 className="text-black font-black text-lg">
-                    {notification.name}
-                    <span className="ml-2 text-lightGray">
-                      {notification.action}
-                      {notification.content && (
-                        <span className="ml-2">{notification.content}</span>
+                  <div className="flex items-center">
+                    <div className="">
+                      <h3 className="text-black font-black text-lg">
+                        {notification.name}
+                        <span className="ml-2 text-lightGray">
+                          {notification.action}
+                          {notification.content && (
+                            <span className="ml-2">{notification.content}</span>
+                          )}
+                        </span>
+                      </h3>
+                      {notification.group && (
+                        <p className="text-blue font-bold text-lg">
+                          {notification.group}
+                        </p>
                       )}
-                    </span>
-                  </h3>
-                  {notification.group && (
-                    <p className="ml-2 text-blue font-bold text-lg">
-                      {notification.group}
-                    </p>
-                  )}
-                  {notification.isUnread && (
-                    <div className="w-2 h-2 bg-red-700 rounded-full ml-3"></div>
-                  )}
+                    </div>
+                  </div>
                 </div>
+                <div className="text-mdGray">
+                  <p>{notification.time}</p>
+                </div>
+                {notification.text && (
+                  <div className="border-2 p-5 mt-3">{notification.text}</div>
+                )}
+                {notification.userPicture && (
+                  <img
+                    src={notification.userPicture}
+                    alt="User picture"
+                    className="w-[45px] h-[45px] mt-3"
+                  />
+                )}
               </div>
-              <div className="text-mdGray">
-                <p>{notification.time}</p>
-              </div>
-              {notification.text && (
-                <div className="border-2 p-5 mt-3">{notification.text}</div>
-              )}
-              {notification.userPicture && (
-                <img
-                  src={notification.userPicture}
-                  alt="User picture"
-                  className="w-[45px] h-[45px] mt-3"
-                />
-              )}
             </div>
+            {notification.isUnread && (
+              <div className="relative flex-shrink-0 top-0 right-0 w-[0.5rem] h-[0.5rem] bg-red-700 rounded-full"></div>
+            )}
           </div>
         </div>
       ))}
